adb7cdc7905aa7cc0931407785974df5005b24c4
Luke Aimson <l.m.aimson@gmail.com>
2023-04-24 18:42:08 +0100

    Added a few commands to macos.sh and fixed spelling mistake in html5.html

--- tools/macos.sh
+++ tools/macos.sh
@@ -1,1 +1,8 @@
-sudo lsof -i -P | grep LISTEN # List all processes running on a specific port
+# List all processes running on a specific port
+sudo lsof -i -P | grep LISTEN 
+# Show hidden files in finder (Permanently)
+defaults write com.apple.finder AppleShowAllFiles True; killall Finder 
+# Hide hidden files in finder (Permanently) 
+defaults write com.apple.finder AppleShowAllFiles False; killall Finder
+# Change default location of screenshots 
+defaults write com.apple.screencapture location <Path> # Or drag folder into terminal 
